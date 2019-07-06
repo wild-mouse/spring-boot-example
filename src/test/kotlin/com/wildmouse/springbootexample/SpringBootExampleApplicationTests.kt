36abab63def9040a9825a23f752ef98a73662a92
Tsukasa Noguchi <wildmouse0813@gmail.com>
2019-07-06 10:49:58 +0900

    First commit

---
+++ src/test/kotlin/com/wildmouse/springbootexample/SpringBootExampleApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.wildmouse.springbootexample
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SpringBootExampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
